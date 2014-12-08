5af1a9a6dfa5d6f61dd9a3616293a43b65673aff
combs <ccombs@gmail.com>
2014-12-07 22:28:20 -0500

    quiet output

--- pi-commands/GIFFR/bmps2raw.sh
+++ pi-commands/GIFFR/bmps2raw.sh
@@ -3,5 +3,5 @@ COMMAND='avconv'
 type avconv >/dev/null 2>&1 || type ffmpeg >2/dev/null 2>&1 && COMMAND='ffmpeg' || { echo >&2 "Requires ffmpeg or avconv but neither is installed.  Aborting."; exit 1; }
 cd `dirname $0`
 for arg in $1*.bmp
-do $COMMAND -y -loglevel quiet -vcodec bmp -i $arg -vcodec rawvideo -f rawvideo -pix_fmt rgb565 `echo $arg|sed s:\....$:.raw:`
+do $COMMAND -y -loglevel quiet -stats off -vcodec bmp -i $arg -vcodec rawvideo -f rawvideo -pix_fmt rgb565 `echo $arg|sed s:\....$:.raw:` 2>/dev/null && rm $arg
 done
