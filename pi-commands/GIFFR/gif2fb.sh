c0e442748c17e0a9bed432fdcdb4cb667f813304
combs <ccombs@gmail.com>
2014-12-07 23:16:18 -0500

    libav doesn't demux gifs

--- pi-commands/GIFFR/gif2fb.sh
+++ pi-commands/GIFFR/gif2fb.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-avconv -y -loglevel quiet -vcodec gif -i $1 -vf scale=320:240 -vcodec rawvideo -f rawvideo -pix_fmt rgb565 `echo $1|sed s:\....$:\%2d.raw:`
+ffmpeg -y -loglevel quiet -vcodec gif -i $1 -vf scale=320:240 -vcodec rawvideo -f rawvideo -pix_fmt rgb565 `echo $1|sed s:\....$:\%2d.raw:`
