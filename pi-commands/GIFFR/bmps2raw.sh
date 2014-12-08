40c1b83376eeaa5e3ff261a9c491fa27f5e830ad
combs <ccombs@gmail.com>
2014-12-07 22:23:17 -0500

    avconv or ffmpeg? now agnostic

--- pi-commands/GIFFR/bmps2raw.sh
+++ pi-commands/GIFFR/bmps2raw.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+COMMAND='avconv'
+type avconv >/dev/null 2>&1 || type ffmpeg >2/dev/null 2>&1 && COMMAND='ffmpeg' || { echo >&2 "Requires ffmpeg or avconv but neither is installed.  Aborting."; exit 1; }
 cd `dirname $0`
 for arg in $1*.bmp
-do avconv -y -loglevel quiet -vcodec bmp -i $arg -vcodec rawvideo -f rawvideo -pix_fmt rgb565 `echo $arg|sed s:\....$:.raw:`
+do $COMMAND -y -loglevel quiet -vcodec bmp -i $arg -vcodec rawvideo -f rawvideo -pix_fmt rgb565 `echo $arg|sed s:\....$:.raw:`
 done
