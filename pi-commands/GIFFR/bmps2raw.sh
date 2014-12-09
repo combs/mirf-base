063b561a633215f3aaec4794653ab5514d784251
combs <ccombs@gmail.com>
2014-12-08 22:25:31 -0500

    remove debug

--- pi-commands/GIFFR/bmps2raw.sh
+++ pi-commands/GIFFR/bmps2raw.sh
@@ -9,7 +9,7 @@ for arg in $1*.bmp
 do echo -n "";
 
 outfile="$2`basename $arg|sed s:\....$:.raw:`"
-
+ 
 $COMMAND -y -loglevel quiet -vcodec bmp -i $arg -vcodec rawvideo -f rawvideo -pix_fmt rgb565 $outfile  2>/dev/null >/dev/null && rm $arg
 
 done
