3de8c7e102b3cbdce5200cb9c818927e1d507a06
combs <ccombs@gmail.com>
2015-02-01 17:06:49 -0500

    prepare for watchdog timering.
    timer no timer

--- arduino-clients/BUSES/mirfscreenconfig.h
+++ arduino-clients/BUSES/mirfscreenconfig.h
@@ -5,7 +5,8 @@ char nameClient[6] = "BUSES";
 
 const long msOnTime=118L*1000;
 const long msTimeout=5000;
-const long msMaxDataAge=30L*1000;
+const long secondsMaxDataAge=30L;
+const long msMaxDataAge=secondsMaxDataAge*1000L;
 
 #define LCD_CLEAR_ON_SLEEP
 //#define LCD_UPDATES_EXTEND_ON_TIME
--- arduino-clients/WCLKK/mirfscreenconfig.h
+++ arduino-clients/WCLKK/mirfscreenconfig.h
@@ -5,7 +5,8 @@ char nameClient[6] = "WCLKK";
 
 const long msOnTime=30L*1000;
 const long msTimeout=5000;
-const long msMaxDataAge=10L*60L*1000;
+const long secondsMaxDataAge=10*60L;
+const long msMaxDataAge=secondsMaxDataAge*1000;
 
 // #define LCD_CLEAR_ON_SLEEP
 #define LCD_UPDATES_EXTEND_ON_TIME
