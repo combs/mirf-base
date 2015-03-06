30e1f064abadeda714bdfa67e1098fff22205a2c
combs <ccombs@gmail.com>
2015-03-05 23:32:35 -0500

    wdt library; actually enable wdt

--- arduino-clients/CHAOS/Mirf_CHAOS.ino
+++ arduino-clients/CHAOS/Mirf_CHAOS.ino
@@ -11,6 +11,7 @@ void setup();
 
 #include "mirfscreenconfig.h"
 
+#include "watchdog.h"
 #include "MirfClient.h"
 
 
@@ -82,6 +83,9 @@ void setup()
   SendToBase("Started");
   delay(100); // Avoid message-spamming race condition at startup
 
+  setup_watchdog(WDTO_1S);
+
+
 
 }
 
@@ -122,6 +126,7 @@ void loop()
     if ( ( ( secondsSinceStartup - secondsGotUpdate ) > secondsMaxDataAge ) && 
       ( ( secondsSinceStartup - secondsRequestedUpdate ) > secondsTimeout ) ) {
       secondsRequestedUpdate=secondsSinceStartup;
+ 
       requestUpdate();
       // screen is on, data is old, but let's not flagUpdate because that will keep screen on longer
 
@@ -190,7 +195,6 @@ void loop()
       case 'S':
         // Sleep
         sleepRequested=true;
-
         updateRequested=true;
         break;
 
@@ -388,41 +392,7 @@ void noBacklight() {
 
 
 
-
-void setup_watchdog(int timerPrescaler) {
-
-  if (timerPrescaler > 9 ) timerPrescaler = 9; //Correct incoming amount if need be
-
-  byte bb = timerPrescaler & 7; 
-  if (timerPrescaler > 7) bb |= (1<<5); //Set the special 5th bit if necessary
-
-#ifdef __AVR_ATmega328P__
-  MCUSR &= ~(1<<WDRF); //Clear the watch dog reset
-  WDTCSR |= (1<<WDCE) | (1<<WDE); //Set WD_change enable, set WD enable
-  WDTCSR = bb; //Set new watchdog timeout value
-  WDTCSR |= _BV(WDIE); //Set the interrupt enable, this will keep unit from resetting after each int
-
-#elif defined __AVR_ATtiny85__
-
-  // attiny
-
-  //This order of commands is important and cannot be combined
-  MCUSR &= ~(1<<WDRF); //Clear the watch dog reset
-  WDTCR |= (1<<WDCE) | (1<<WDE); //Set WD_change enable, set WD enable
-  WDTCR = bb; //Set new watchdog timeout value
-  WDTCR |= _BV(WDIE); //Set the interrupt enable, this will keep unit from resetting after each int
-
-#else 
-  //??
-#error "I don't know how to handle your AVR in setup_watchdog"
-#endif
-
-
-}
-
-
-
-
+ 
 
 
 
