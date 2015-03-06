f76a4e9296753ea50ff89f4f9172d6dc604fb376
combs <ccombs@gmail.com>
2015-03-05 22:38:48 -0500

    Bugfixes, faster response time, standardize

--- arduino-base/Mirf_Base.ino
+++ arduino-base/Mirf_Base.ino
@@ -13,7 +13,7 @@ char nameClient[6] = "BASES";
 String inputString = "";         // a string to hold incoming data
 boolean stringComplete = false;  // whether the string is complete
 
-long msUpdateInterval=500;
+const long msUpdateInterval=250;
 long msLastUpdate=0;
 
 
@@ -32,8 +32,8 @@ void setup()
   Mirf.setRADDR((byte *)"BASES");
 
   Mirf.payload=32; 
-  
-  Mirf.channel = 10;
+
+  Mirf.channel=10;
   // configure 15 retries, 250us between attempts
   Mirf.configRegister(SETUP_RETR,(B0000<<ARD ) | (B1111<<ARC));
 
@@ -44,45 +44,47 @@ void setup()
 
   // Set 1MHz data rate - this increases the range slightly
   Mirf.configRegister(RF_SETUP,0x06);
-  
-//  Mirf.configRegister(RF_SETUP, (B0000<<RF_DR_HIGH) | (B
 
-  Mirf.setTADDR((byte *)"WCLKK");
+  //  Mirf.configRegister(RF_SETUP, (B0000<<RF_DR_HIGH) | (B
+ 
+  SendMessage("WCLKKBASES0Base station");
+  SendMessage("WCLKKBASES1initialized");
 
-  Mirf.config();
-  Mirf.send((byte *)"BASES0Base station");
-  blockForSend();
-  Mirf.config();
-  delay(100);
-  Mirf.send((byte *)"BASES1initialized");
-  blockForSend();
-  delay(1000);
-  Serial.print("BASES BASES Booted ");
+  Serial.print(nameClient);
+  Serial.print(" ");
+  Serial.print(nameBase);
+  Serial.print(" Booted " );
   Serial.println(Mirf.getStatus());
-  delay(1000);
+  
+  Mirf.powerUpRx();
+
+
 }
 
 void loop()
 { 
-    
+
   if (millis() - msLastUpdate > msUpdateInterval ) {
     msLastUpdate=millis();
-    Serial.println("BASES BASES UPDATE");
-    Serial.flush();
-    
-  }
-  
 
-  byte data[Mirf.payload];
+    Serial.print(nameClient);
+    Serial.print(" ");
+    Serial.print(nameBase);
+    Serial.println(" UPDATE " );
+    Serial.flush();
+
+  }
+
+
 
   // is there any data pending? 
   if( Mirf.dataReady() )
   {
+    byte data[Mirf.payload];
     Mirf.getData((byte *) &data);
 
     // ... and write it out to the PC
-    char theFrom[6];
-    char theTo[6]="BASES";
+    char theFrom[6]; 
     char theMessage[Mirf.payload];
     String thePayload=String((char *)data);
     //thePayload=*data;
@@ -92,48 +94,41 @@ void loop()
     thePayload.substring(0,5).toCharArray(theFrom,6);
     thePayload.substring(5).toCharArray(theMessage,Mirf.payload);
 
-    Serial.print(theTo);
+    Serial.print(nameBase);
     Serial.print(" ");
     Serial.print(theFrom);
     Serial.print(" ");
     Serial.println(theMessage);
     Serial.flush();
-    msLastUpdate=millis();
+    msLastUpdate=millis(); 
+
   }
-  
-  
+
+
   while (Serial.available() > 0) {
     // get the new byte:
     char inChar = (char)Serial.read(); 
     // add it to the inputString:
-    // if the incoming character is a newline, set a flag
-    // so the main loop can do something about it:
+    // if the incoming character is a newline, set a flag 
+    
     if (inChar == '\n' || inChar == '\r') {
       stringComplete = true;
       break;
-    } else {
+    } 
+    else {
       inputString += inChar;
     }
-    
+
   }
-  
+
   if (stringComplete==true) {
-    
+
     if (inputString.length()>9){ 
-      char theTarget[6];
-      char theMessage[Mirf.payload];
-      inputString.substring(0,5).toCharArray(theTarget,6);
-      inputString.substring(5).toCharArray(theMessage,Mirf.payload);
-      Mirf.setTADDR((byte *)theTarget);
-      Mirf.setRADDR((byte *)"BASES");
-      Mirf.config();
-      Mirf.powerUpTx();
-      Mirf.send((byte *)theMessage);
-      blockForSend();
-      Mirf.powerUpRx();
-      
+
+      SendMessage(inputString);
+
       Serial.print("BASES BASES Message_sent_to ");
-      Serial.println(theTarget);
+      Serial.println(inputString.substring(0,5));
       Serial.flush();
       msLastUpdate=millis();
     }
@@ -142,11 +137,14 @@ void loop()
     inputString="";
 
   }
+
+
+  blockForSend();
   
 
-    blockForSend();
 
-       
 }
- 
- 
+
+
+
+
