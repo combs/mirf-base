041ddac050e15df14ffbe9ff012976d8737816c4
combs <ccombs@gmail.com>
2015-04-05 14:46:55 -0400

    Formatting improvements; use global functions

---
+++ pi-commands/WCLKK/alerts.php
@@ -0,0 +1,48 @@
+<?php
+
+
+$alert_priorities = array(
+"HUR" => 100,
+"TOR" => 95,
+"TOW" => 90,
+"WRN" => 85,
+"SEW" => 80,
+"VOL" => 75,
+"WIN" => 13, // this will be shown in precip
+"FLO" => 10, // flooding doesn't concern us
+"WAT" => 10, // flooding doesn't concern us
+"WND" => 52,
+"SVR" => 60,
+"HEA" => 10, // this will be shown in temps
+"FOG" => 20,
+"SPE" => 25,
+"FIR" => 50,
+"HWW" => 98,
+"REC" => 15, // oooOOoo a record
+"PUB" => 0, 
+"REP" => 0 // what are these even for
+);
+$alert_texts = array(
+"HUR" => "HURRICANE ALERT",
+"TOR" => "TORNADO WARNING",
+"TOW" => "Tornado Watch",
+"WRN" => "Severe Storm",
+"SEW" => "Storm Watch",
+"VOL" => "Volcano Warning",
+"WIN" => "Winter Weather",
+"FLO" => "Flood Warning",
+"WAT" => "Flood Watch",
+"WND" => "Alert: High Wind",
+"SVR" => "Severe Weather",
+"HEA" => "Heat Advisory",
+"FOG" => "Dense Fog",
+"SPE" => "Special Alert",
+"FIR" => "Fire Watch",
+"HWW" => "HURRICANE WIND",
+"REC" => "Record Just Set",
+"PUB" => "Public Report",
+"REP" => "NWS Says Hi"
+);
+
+
+?>
