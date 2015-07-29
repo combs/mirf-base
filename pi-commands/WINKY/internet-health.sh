a87e4cc8ffd3af73cda89e06d7c6c5d2ca283e79
combs <ccombs@gmail.com>
2015-07-28 23:17:52 -0400

    better method

--- pi-commands/WINKY/internet-health.sh
+++ pi-commands/WINKY/internet-health.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+cd `dirname $0`
+. ../common/get-directories
 
 ping -q -c 1 comcast.com >/dev/null 2>/dev/null
 if [ $? -gt 0 ] 
 then
 
-curl 'http://dc.chriscombs.net:10080/color.php?speed=1&mode=p&color0=%23500a00&color1=%23000000' >/dev/null 2>/dev/null
+echo 'WINKYBASESp4"    ' > $BASEDIR/out/internet-health
 touch /tmp/cache-mirf-internet-health
 
 elif [ -e /tmp/cache-mirf-internet-health ]
 then
-curl 'http://dc.chriscombs.net:10080/color.php?speed=1&mode=f&color0=%230e4d00&color1=%23000000' >/dev/null 2>/dev/null
+echo 'WINKYBASESf#3    ' > $BASEDIR/out/internet-health
+
 rm /tmp/cache-mirf-internet-health
 fi
 
