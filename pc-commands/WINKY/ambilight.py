658fe3633e8cfb5d4afa62088e7cfe36e4a4feb4
combs <ccombs@gmail.com>
2015-12-01 22:55:40 -0500

    add image offset calculation (nukes letterboxing)

--- pc-commands/WINKY/ambilight.py
+++ pc-commands/WINKY/ambilight.py
@@ -8,23 +8,26 @@ from scp import SCPClient
 
 
 ourScreenWidth=ImageGrab.grab().size[0]
-
+ourOffset=0
 ourStrip=8
 
 def millis():
 	return int(round(time.time() * 1000))
 
 def getChunk(chunk,ourImage):
+	global ourOffset
 	if chunk > (ourStrip-1):
 		return (0,0,0,0)
 	
 	ourStripWidth=ourScreenWidth / (ourStrip)
 	
-#	return (0,0,0,1)
-	ourCrop=ourImage.crop((ourStripWidth*chunk,00,ourStripWidth*(chunk+1),100))
-	ourMath=ImageStat.Stat(ourCrop)
-	color=(round(ourMath.mean[0]),round(ourMath.mean[1]),round(ourMath.mean[2]),round(ourMath.mean[3]))
-
+	ourCrop=ourImage.crop((ourStripWidth*chunk,0,ourStripWidth*(chunk+1),100))
+	ourMath=ImageStat.Stat(ourCrop)  
+	color=(round(ourMath.rms[0]),round(ourMath.rms[1]),round(ourMath.rms[2]),round(ourMath.rms[3])) 
+	
+	if (color[0]<9 and color[1]<9 and color[2] < 9 and color[0]>0 and color[1]>0 and color[2]>0):
+		color[0]=color[1];
+		color[2]=color[1]; 
 	
 	return color
 
@@ -36,6 +39,19 @@ def encodeColorChannel(channel):
 	channel=int(round(channel)) >> 2
 	channel+=32
 	return chr(channel)
+
+def getOffset(ourImage):
+	global ourOffset
+	ourX=int(round(ourImage.size[0]/2))
+	print "sampling at X: " +str(ourX)
+	for chunk in range(1,40):
+		ourCrop=ourImage.crop((ourX,(chunk-1)*10,ourX+1,chunk*10)) 
+		ourMath=ImageStat.Stat(ourCrop)
+		pprint(ourMath.mean)
+		color=( round(ourMath.mean[0]),round(ourMath.mean[1]),round(ourMath.mean[2]),round(ourMath.mean[3]) )
+		if (color[0]>0 or color[1]>0 or color[2]>0):
+			ourOffset=(chunk-1)*10
+			break
 
 s = sched.scheduler(time.time, time.sleep)
 
@@ -50,7 +66,7 @@ lastMillis=millis();
 
 
 def doLoop(sc) :
-	global iterator, enabled, ourScreenWidth, lastMillis
+	global iterator, enabled, ourScreenWidth, lastMillis, ourOffset
 	
 	iterator += 1
 	
@@ -60,20 +76,29 @@ def doLoop(sc) :
 				enabled=0
 			else:
 				enabled=1
+					
+			bigImage=ImageGrab.grab(bbox=(0,0,ourScreenWidth,440))
+			ourScreenWidth=bigImage.size[0]
+			print "screen size: " + str(ourScreenWidth)
+			getOffset(bigImage)
+			print "letterbox offset: " + str(ourOffset)
+			
 		else:
-			enabled=1
-				
-		ourScreenWidth=ImageGrab.grab().size[0]
+		
+			bigImage=ImageGrab.grab(bbox=(0,00,ourScreenWidth,440))
+			getOffset(bigImage)
+			print(ourOffset)
+			
+			print "sleeping..."
 
 	if (enabled):
 				
 		sc.enter(0.2, 1, doLoop, (sc,))
 		colors = [0 for i in xrange(ourStrip)] 
 		colorCommand="WINKYBASESA9"
-		ourImage=Image
-				
+
 		try:
-			ourImage=ImageGrab.grab(bbox=(0,00,ourScreenWidth,100))
+			ourImage=ImageGrab.grab(bbox=(0,ourOffset,ourScreenWidth,ourOffset+100))
 				
 			for chunk in reversed(range(len(colors))):
 				colors[chunk]=getChunk(chunk,ourImage)
