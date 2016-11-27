5aba874e01356231461cbb279a97f511a081a518
combs <ccombs@gmail.com>
2016-11-27 10:39:21 -0500

    from & to

--- pi-commands/STATS/update-traffic-deaths.sh
+++ pi-commands/STATS/update-traffic-deaths.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-curl http://www.icebike.org/real-time-traffic-accident-statistics/ 2>/dev/null | grep count_ | head -1 | sed -e 's:.* = ::' -e 's:;.*::' -e 's:^:V:'
+
+cd `dirname $0`
+. ../common/get-directories
+
+curl http://www.icebike.org/real-time-traffic-accident-statistics/ 2>/dev/null | grep count_ | head -1 | sed -e 's:.* = ::' -e 's:;.*::' -e "s:^:V:" -e "s:^:$FROM$BASE:"
--- pi-commands/STATS/update.sh
+++ pi-commands/STATS/update.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-curl 'http://interactive.guim.co.uk/2015/the-counted/v/1460319534281/js/main.js' 2>/dev/null | grep 'var total$1' | sed -e 's:.* = ::' -e 's:;.*::' -e 's:^:V:'
+
+cd `dirname $0`
+. ../common/get-directories
+
+curl 'http://interactive.guim.co.uk/2015/the-counted/v/1460319534281/js/main.js' 2>/dev/null | grep 'var total$1' | sed -e 's:.* = ::' -e 's:;.*::' -e 's:^:V:' -e "s:^:$FROM$BASE:"
