5d251e6d95de44966182b2d11d77655f23254c4d
combs <ccombs@gmail.com>
2016-11-27 10:31:13 -0500

    value prefix

--- pi-commands/STATS/update-traffic-deaths.sh
+++ pi-commands/STATS/update-traffic-deaths.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl http://www.icebike.org/real-time-traffic-accident-statistics/ 2>/dev/null | grep count_ | head -1 | sed -e 's:.* = ::' -e 's:;.*::' 
+curl http://www.icebike.org/real-time-traffic-accident-statistics/ 2>/dev/null | grep count_ | head -1 | sed -e 's:.* = ::' -e 's:;.*::' -e 's:^:V:'
