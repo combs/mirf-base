470aa9fa181697554e436eeaf6b64d946d745476
combs <ccombs@gmail.com>
2016-11-27 11:30:32 -0500

    trailing \n

--- pi-commands/STATS/update-traffic-deaths.sh
+++ pi-commands/STATS/update-traffic-deaths.sh
@@ -4,3 +4,4 @@ cd `dirname $0`
 . ../common/get-directories
 
 curl http://www.icebike.org/real-time-traffic-accident-statistics/ 2>/dev/null | grep count_ | head -1 | sed -e 's:.* = ::' -e 's:;.*::' -e "s:^:V:" -e "s:^:$FROM$BASE:"
+echo ""
