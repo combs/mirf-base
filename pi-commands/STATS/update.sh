27aa16a1b7d7e18d58554a87ff9607296685058e
combs <ccombs@gmail.com>
2016-11-27 11:20:35 -0500

    opps

--- pi-commands/STATS/update.sh
+++ pi-commands/STATS/update.sh
@@ -3,6 +3,6 @@
 cd `dirname $0`
 . ../common/get-directories
 
-curl 'http://interactive.guim.co.uk/2015/the-counted/v/1460319534281/js/main.js' 2>/dev/null | grep 'var total$1' | sed -e 's:.* = ::' -e 's:;.*::' -e 's:^:V:' -e "s:^:$FROM$BASE:"
+# curl 'http://interactive.guim.co.uk/2015/the-counted/v/1460319534281/js/main.js' 2>/dev/null | grep 'var total$1' | sed -e 's:.* = ::' -e 's:;.*::' -e 's:^:V:' -e "s:^:$FROM$BASE:"
 
 curl 'https://interactive.guim.co.uk/2015/the-counted/thrasher-2016.json' 2>/dev/null | sed -e 's/.*://' -e 's:}.*::' -e 's:^:V:' -e "s:^:$FROM$BASE:"
