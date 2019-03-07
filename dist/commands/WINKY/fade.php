0dfa473cdcf94a9395f4c9bd65feae1f38cbe628
Chris Combs <ccombs@gmail.com>
2019-03-06 19:38:32 -0500

    ambient information display

---
+++ dist/commands/WINKY/fade.php
@@ -0,0 +1,19 @@
+<?php
+
+// Standard includes
+
+$our_include_path=dirname(__FILE__) . ":" . dirname(__FILE__) . "/../common" . ":" . GETENV("HOME");
+set_include_path($our_include_path);
+require "get-directories.php";
+
+$currentHour=date('H');
+$currentMinute=date('i') + $currentHour*60;
+$desiredColor=array(0,0,0);
+$nameClient="WINKY";
+
+$stringOutput="f";
+$stringOutput.=encodeColor($details[0],$details[1],$details[2]);
+
+send_to_client($stringOutput);
+
+?>
