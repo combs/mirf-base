d77e30270a6fb4ce61aef233854757666d746e4c
combs <ccombs@gmail.com>
2019-10-29 09:10:22 -0400

    update internet health

--- pi-commands/WINKY/internet-health.sh
+++ pi-commands/WINKY/internet-health.sh
@@ -2,13 +2,13 @@
 cd `dirname $0`
 . ../common/get-directories
 
-ping -q -c 2 bing.com >/dev/null 2>/dev/null
+ping -q -c 2 edge.markovradio.stream >/dev/null 2>/dev/null
 if [ $? -gt 0 ] 
 then
 
 echo 'WINKYBASESp4"    ' > $BASEDIR/out/internet-health
 touch /tmp/cache-mirf-internet-health
-echo "Failed to ping bing.com twice within 2 second at `date`" >> ~/.internet-health.log
+echo "Failed to ping edge.markovradio.stream twice within 2 second at `date`" >> ~/.internet-health.log
 
 elif [ -e /tmp/cache-mirf-internet-health ]
 then
